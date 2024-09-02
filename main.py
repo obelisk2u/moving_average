fa7f2c44887fe41445844a290370de34e2e5b250
Jordan <jdstout@gmail.com>
2024-09-02 16:08:20 -0400

    changed to pandas df (i think) and implemented MA

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ def read_csv(filename):
     df = df.sort_values(by = 'Date')
     return df
 
+def moving_avg(df, dt):
+    df['MA']=df.rolling(window=dt, min_periods=0)['Open'].mean()
+    print(df.iloc[0:100,7])
+    return df
+
+
 def main():
     df=read_csv("TSLA.csv")
-    
+    df=moving_avg(df, 100)
+    df.plot(x = 'Date', y = 'MA')
+
+    plt.show()
+
 
 
 if __name__ == '__main__':

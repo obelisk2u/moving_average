b40154e3b62f38d2c349434e2f3dbe569a9a0d1c
Jordan <jdstout@gmail.com>
2024-09-02 17:12:20 -0400

    infinite money hack unlocked

--- main.py
+++ main.py
@@ -3,32 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 
-
-
-def analyze_data(df, period):
-    #find avg of daily high/low
-    df['hc_avg']=(df['Open']+df['Close'])/2
-
-    #take moving average 
-    df['MA']=df.rolling(window=period, min_periods=0)['hc_avg'].mean()
-
-    #take slope of moving average
-    df["Slope"]= df['MA'].diff()
-
-    return df
-
 def plot(df, xin, yin):
     df.plot(x=xin, y=yin)
     plt.show
 
+def analyze_data(df, period):
+    #find avg of daily high/low
+    df['oc_avg']=(df['Open']+df['Close'])/2
+
+    #take moving average 
+    df['MA']=df.rolling(window=period, min_periods=0)['oc_avg'].mean()
+
+    #take discrete derivative of moving average
+    df["Slope"] = df['MA'].diff()
+
+    #take discrete second derivative of moving average
+    df['Accel'] = df['Slope'].diff()
+
+    return df
+
+def buy_sell(df):
+    cash_pos=0
+    profit=0
+    for i in range(0,len(df)):
+        if(df['Accel'][i]>0):
+            cash_pos = cash_pos + df["oc_avg"][i]
+        else:
+            profit+=cash_pos
+            cash_pos=0
+    print(profit)
 
 def main():
     df = pd.read_csv('TSLA.csv')
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.sort_values(by = 'Date')
     df = analyze_data(df, 75)
-    plot(df,'Date', 'Slope')
-    plt.show()
+    buy_sell(df)
+    #plot(df,'Date', 'Accel')
+    #plt.show()
 
 
 
